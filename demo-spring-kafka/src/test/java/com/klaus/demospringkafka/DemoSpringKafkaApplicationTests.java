92964024315aa01782abf2aa5c727c84c45e0307
klaus mikaelson <love20141002@126.com>
2019-03-01 20:52:02 +0800

    [faeture] add test for jooq

--- demo-spring-kafka/src/test/java/com/klaus/demospringkafka/DemoSpringKafkaApplicationTests.java
+++ demo-spring-kafka/src/test/java/com/klaus/demospringkafka/DemoSpringKafkaApplicationTests.java
@@ -10,7 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
 import static com.generator.tables.User.USER;
+import static org.jooq.impl.DSL.count;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
@@ -24,11 +28,23 @@ public class DemoSpringKafkaApplicationTests {
 
     @Test
     public void testGetUser() {
-
+        Integer count = create.select(count(USER.NAME)).from(USER).fetchOne(0, Integer.class);
         Result<UserRecord> result = create.selectFrom(USER).fetch();
         log.info("data is {}", result);
+        log.info("count is {}" , count);
+        assertEquals(count.intValue(), result.size());
+    }
 
-        assertEquals(1, result.size());
+
+    @Test
+    public void testInsert() {
+
+        log.info("local zone id is {}, now is {}", ZoneId.of("GMT+8"), LocalDateTime.now().atZone(ZoneId.of("GMT+8")));
+        UserRecord userRecord = create.insertInto(USER, USER.NAME, USER.BIRTH_DAY, USER.BIRTH_TIME, USER.BIRTH_SECONDS)
+                .values("damon salvatore", LocalDateTime.now(ZoneId.of("GMT+8")), LocalDateTime.now(ZoneId.of("GMT+8")), (long) LocalDateTime.now(ZoneId.of("GMT+8")).getNano())
+                .returning()
+                .fetchOne();
+        log.info("userRecord is {}", userRecord.getValue(USER.ID));
 
     }
 
